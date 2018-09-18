16e12e7b246023f4120d25048c8cb083f57b9210
oguzbakir <b21627007@cs.hacettepe.edu.tr>
2018-09-18 19:42:52 +0300

    Added dog cat network

--- Dog and Cat Recognition/dog_cat.py
+++ Dog and Cat Recognition/dog_cat.py
@@ -1,56 +1,33 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import os
-import cv2
-import random
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
 import pickle
 
-DATADIR = "./PetImages"
-CATEGORIES = ["Dog", "Cat"]
+X = pickle.load(open("X.pickle", "rb"))
+y = pickle.load(open("y.pickle", "rb"))
 
-IMG_SIZE = 50  # Every image is normalized to IMG_SIZExIMG_SIZE
+X = X/255.0  # Normalize
 
-img_array = []
-training_data = []
+model = Sequential()
+model.add(Conv2D(64, (3,3), input_shape=X.shape[1:]))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2,2)))
 
+model.add(Conv2D(64, (3,3)))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2,2)))
 
-def create_training_data():
-    for category in CATEGORIES:
-        path = os.path.join(DATADIR, category)  # Path to cats or dogs dir
-        class_num = CATEGORIES.index(category)
-        for img in os.listdir(path):  # For every image in directory
-            try:
-                img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)  # Convert images to grayscale array
-                resized_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-                training_data.append([resized_array, class_num])
-            except Exception as e:
-                pass
+model.add(Flatten())  # This converts our 3D feature maps to 1D feature vectors
 
+model.add(Dense(64))
+model.add(Activation('relu'))
 
-create_training_data()  # OK, we have data but its half dog half cat sequentially.
-#  We have to shuffle it.
-random.shuffle(training_data)
+model.add(Dense(1))  # Output layer
+model.add(Activation('sigmoid'))  # Activation for output layer
 
-X = []  # Feature set
-y = []  # Labels
+model.compile(loss="binary_crossentropy",
+              optimizer="adam",
+              metrics=['accuracy'])
 
-for features, label in training_data:
-    X.append(features)
-    y.append(label)
-
-X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-
-# Dump arrays with pickle
-pickle_out = open("X.pickle", "wb")
-pickle.dump(X, pickle_out)
-pickle_out.close()
-
-pickle_out = open("y.pickle", "wb")
-pickle.dump(y, pickle_out)
-pickle_out.close()
-
-"""To load them use
-pickle_in = open("X.pickle", "rb")
-X = pickle.load(pickle_in)
-"""
+model.fit(X, y, batch_size=32, epochs=3, validation_split=0.3)  # validation_split is percentage to be used to confirm
 
---
+++ Dog and Cat Recognition/input_gen.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+import random
+import pickle
+
+DATADIR = "./PetImages"
+CATEGORIES = ["Dog", "Cat"]
+
+IMG_SIZE = 50  # Every image is normalized to IMG_SIZExIMG_SIZE
+
+img_array = []
+training_data = []
+
+
+def create_training_data():
+    for category in CATEGORIES:
+        path = os.path.join(DATADIR, category)  # Path to cats or dogs dir
+        class_num = CATEGORIES.index(category)
+        for img in os.listdir(path):  # For every image in directory
+            try:
+                img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)  # Convert images to grayscale array
+                resized_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+                training_data.append([resized_array, class_num])
+            except Exception as e:
+                pass
+
+
+create_training_data()  # OK, we have data but its half dog half cat sequentially.
+#  We have to shuffle it.
+random.shuffle(training_data)
+
+X = []  # Feature set
+y = []  # Labels
+
+for features, label in training_data:
+    X.append(features)
+    y.append(label)
+
+X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+
+# Dump arrays with pickle
+pickle_out = open("X.pickle", "wb")
+pickle.dump(X, pickle_out)
+pickle_out.close()
+
+pickle_out = open("y.pickle", "wb")
+pickle.dump(y, pickle_out)
+pickle_out.close()
+
+"""To load them use
+pickle_in = open("X.pickle", "rb")
+X = pickle.load(pickle_in)
+"""
+
